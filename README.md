bb1fd4f1ff75bc57024fda8ddf7170e1ad744ef5
oramirezperera <ramirez.orlando17@gmail.com>
2021-08-16 16:36:02 -0400

    added the files of the project and make some changes to the readme and requirements.txt file, added the environment to the .gitignore file

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # flask_test
-An API made in Flask
+An API made in Flask.
+
+This API has two main branches jokes and numbers:
+
+### Jokes 
+If you pass a path use the route '/jokes' you will get a random joke. Instead if you pass a path paragram with the word Chuck or the word Dad you will get a Chuck Norrris joke from the [Chuck Norris API](https://api.chucknorris.io) or a dad joke from [Dad jokes API](https://icanhazdadjoke.com/api).
+
+### Numners
+
+- Using the path /numbers you must pass a list of integers as a path parameter this will return the least common multiple of the numbers.
+
+- Using the path /number you must pass an integer and this will return the given number + 1.
+
+## Starting
+
+You will need to clone this repository 
+
+### Prerequirements
+- You will need Python 3.X installed on your computer
+- You will need some Python libraries such as requests and Flask.
+
+## Installation
+
+You can install all the requirements of this project using the following code:
+
+- First create a virtual environment, in this example case we will call it:
+
+```Bash
+python3 -m venv flask_test
+```
+
+flask_test is the name of our virtual environment
+
+- Once we have created your virtual environment you must activate it with:
+
+If you are in Linux use:
+```Bash
+$ source flask_test/bin/activate
+```
+If you are in Windows use:
+```Bash
+flask_test\Scripts\activate
+```
+
+To deactivate the virtual environment just type:
+```Bash
+deactivate
+```
+
+Now that we have activate our virtual environment we can install all the libraries and dependencies we need using the following code:
+
+```Bash
+pip3 install -r requirements.txt
+```
+
+Once you have installed the libraries needed in this project you can run the flask API using:
+
+```Bash
+$ flask run
+```
+
---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request, url_for
+import re
+from math import gcd
+import requests
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Front page, have links to all the utilities of this app
+    """
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    handling the 404 error not found
+    """
+
+    return render_template('404.html', error=error)
+
+
+@app.route('/number/')
+@app.route('/number/<int:number>')
+def number(number=1):
+    """passing an integer as a parameter in the browser returns the integer plus 1
+
+    If you don't pass any parameter 1 will be the example number
+        
+    example route http://127.0.0.1:5000/number/1 will return 2
+
+    """
+    fin_number = number + 1
+    return render_template('number.html', number=str(number), fin_number=str(fin_number))
+
+
+@app.route('/numbers/')
+@app.route('/numbers/<int_list>')
+def numbers(int_list='2,3'):
+    """
+    Passing a list of numbers separated by ',' you can this function returns the least common multiple of the numbers, can be 2 or more
+
+    If you dont pass any number int_list default is 2,3
+
+    example http://127.0.0.1:5000/numbers/2,3 returns 6
+    """
+    # Verifiying numbers in the list
+    if not re.match(r'^\d+(?:,\d+)*,?$', int_list):
+        return render_template('numbers_error.html')
+    
+    lcm = 1
+    num_list = [int(num) for num in int_list.split(',')]
+    for i in num_list:
+        lcm = i*lcm//gcd(lcm,i)
+    return render_template('numbers.html', int_list=list(num_list), lcm=str(lcm))
+
+
+def chuck():
+    """
+    Function to do the request to the chucknorris jokes API.
+    """
+    r = requests.get('https://api.chucknorris.io/jokes/random')
+    chuck = r.json()
+    joke = chuck["value"]
+    return render_template('chuck.html', joke=joke)
+
+
+def dad():
+    """
+    function to do the request to the dad jokes
+    """
+    r = requests.get('https://icanhazdadjoke.com/', headers={"Accept": "application/json"})
+    dad = r.json()
+    joke = dad["joke"]
+    return render_template('dad.html', joke=joke)
+
+
+@app.route('/jokes/')
+@app.route('/jokes/<string:toj>/')
+def jokes(toj='random'):
+    """
+    Function that takes the parameter Chuck or Dad, to return a Chuck Norris joke or a Dad joke.
+    If no paramaterer is passed returns a random Chuck or dad joke
+    """
+    if toj != 'random':
+        if toj.lower() == 'chuck':
+            return chuck()
+        elif toj.lower() == 'dad':
+            return dad()
+        else:
+            return render_template('joke_error.html', error=str(toj))
+    if toj == 'random':
+        ran = random.randint(1,2)
+        if ran == 1:
+            return chuck()
+        if ran == 2:
+            return dad()
